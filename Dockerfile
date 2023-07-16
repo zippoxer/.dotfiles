425d19876b4e1f6b8a5e9463d549b2ad3e9dedec
moshe-blox <moshe@blox.io>
2023-07-16 16:38:11 +0300

    fix: install script

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM ubuntu:22.04
+
+# Install sudo
+RUN apt-get update -y && apt-get install -y sudo
+
+# Copy your script into the Docker container
+COPY . /root/.dotfiles
+
+# Run your installation script
+RUN /root/.dotfiles/install.sh
+
+CMD ["/bin/bash"]
--- install.sh
+++ install.sh
@@ -1,37 +1,75 @@
-# Install nvm if it's not already installed
-if [ ! -s "${HOME}/.nvm/nvm.sh" ]; then
-    echo "Installing nvm..."
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-else
-    echo "nvm is already installed."
-fi
-# Source nvm to make it available for the script
-[ -s "${HOME}/.nvm/nvm.sh" ] && \. "${HOME}/.nvm/nvm.sh"
-nvm use --lts
+#!/bin/bash
 
-# Install powerlevel10k theme if it's not already installed
-if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" ]; then
-    echo "Installing powerlevel10k theme..."
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-else
-    echo "powerlevel10k theme is already installed."
-fi
+mode=$1
 
-# Install zsh-nvm if it's not already installed
-if [ ! -d "${HOME}/.oh-my-zsh/custom/plugins/zsh-nvm" ]; then
-    echo "Installing zsh-nvm..."
-    git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm
-else
-    echo "zsh-nvm is already installed."
-fi
+print_header() {
+  echo -e "\n\n===================================================="
+  echo "== $1"
+  echo "===================================================="
+}
 
-# Install fzf if it's not already installed
-if [ ! -d "${HOME}/.fzf" ]; then
-    echo "Installing fzf..."
-    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
-else
-    echo "fzf is already installed."
+if [ "$mode" != "postshell" ]; then
+    # Update package lists
+    sudo apt-get update -y
+
+    # Check and install dependencies
+    for cmd in zsh git curl; do
+        command -v $cmd &> /dev/null || to_install+=($cmd)
+    done
+    print_header "Installing dependencies (${to_install[@]})"
+    (( ${#to_install[@]} )) && sudo apt-get install -y ${to_install[@]}
+
+    # Install oh-my-zsh if not present
+    if [ ! -d "${HOME}/.oh-my-zsh" ]; then
+      print_header "Installing oh-my-zsh"
+      sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+    else
+      print_header "oh-my-zsh is already installed"
+    fi
+
+    # Call second stage
+    zsh $0 postshell
 fi
 
-# Copy .zshrc
-cp ~/.dotfiles/.zshrc ~/.zshrc
+if [ "$mode" = "postshell" ]; then
+    # Function to clone a git repo if directory doesn't exist
+    install_from_git() {
+        if [ ! -d "$1" ]; then
+          print_header "Installing $(basename "$1")"
+          git clone $2 $1
+        else
+          print_header "$(basename "$1") is already installed"
+        fi
+    }
+
+    # Install nvm
+    if [ ! -d "${HOME}/.nvm" ]; then
+      print_header "Installing NVM"
+      curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+      [ -s "${HOME}/.nvm/nvm.sh" ] && . "${HOME}/.nvm/nvm.sh"  # source nvm
+      nvm use --lts
+    else
+      print_header "NVM is already installed"
+    fi
+
+    # Install powerlevel10k theme
+    install_from_git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" 'https://github.com/romkatv/powerlevel10k.git'
+
+    # Install zsh-nvm
+    install_from_git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-nvm" 'https://github.com/lukechilds/zsh-nvm'
+
+    # Install zsh-autosuggestions
+    install_from_git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" 'https://github.com/zsh-users/zsh-autosuggestions'
+
+    # Install fzf
+    FZF_DIR="${HOME}/.fzf"
+    install_from_git "$FZF_DIR" 'https://github.com/junegunn/fzf.git'
+    "$FZF_DIR"/install --all
+
+    # Copy .zshrc
+    print_header "Copying .zshrc file"
+    cp ~/.dotfiles/.zshrc ~/.zshrc
+
+    # Replace the current shell with Zsh
+    exec zsh -l
+fi
